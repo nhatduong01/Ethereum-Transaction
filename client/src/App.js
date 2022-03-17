f4e4b1b60c7f8e9539c182627edad8ab285d8b41
DuongNguyen <nhatduong30001@gmail.com>
2022-03-17 10:21:14 +0700

    Add materials

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import "./App.css";
+import { NavBar, Welcome, Footer, Services, Transactions } from "./components";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <NavBar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 }
